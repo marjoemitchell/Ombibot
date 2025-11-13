eced73032818135d6a3dd85e57f4d1b215e1d9c8
marjoemitchell <marjoemitchell@gmail.com>
2025-11-12 20:30:35 -0700

    Add debug logging for environment variables

--- config.ru
+++ config.ru
@@ -3,6 +3,12 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require "dotenv"
 Dotenv.load if File.exist?(".env")
 
+# Debug: Print environment on startup
+puts "=== OmbiBot Container Startup Debug ==="
+puts "Current environment variables:"
+ENV.each { |k, v| puts "  #{k}=#{v[0..50]}..." if k.start_with?("SLACK", "OMBI", "PLEX", "PORT", "RACK", "TZ") }
+puts "========================================"
+
 require "ombibot"
 require "web"
 
--- ombibot.rb
+++ ombibot.rb
@@ -3,6 +3,13 @@ require "ombibot/commands/hello"
 require "ombibot/commands/request"
 require "ombibot/bot"
 
+# Debug: Check if SLACK_API_TOKEN is set
+if ENV["SLACK_API_TOKEN"].nil? || ENV["SLACK_API_TOKEN"].empty?
+  puts "WARNING: SLACK_API_TOKEN is not set. Bot will fail to initialize."
+  puts "Environment variables present:"
+  ENV.each { |k, v| puts "  #{k}=#{v[0..20]}..." if k.start_with?("SLACK", "OMBI", "PLEX", "PORT", "RACK", "TZ") }
+end
+
 SlackRubyBot.configure do |config|
   config.aliases = ["plex", "plex_request", "ombi"]
 end
